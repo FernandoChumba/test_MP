21a2f14f88b41fd67576971cc80318fdccf3b0da
FernandoChumba <68929807+FernandoChumba@users.noreply.github.com>
2024-10-18 00:09:53 -0300

    rutas de mercado pago
    por si sale bien , por si falla y la promesa para obtener las respuestas

--- server/src/router/Mercado_Pago_Router.js
+++ server/src/router/Mercado_Pago_Router.js
@@ -1,17 +1,55 @@
-const {router} = require("express");
+const { router } = require("express");
+
+const mercadopago = require("mercadopago");
+//permite usar variables de entorno
+const dotenv = require("dotenv");
+dotenv.config();
 
 const Mercado_Pago = router();
 
+//configurando mercado pago
 
+mercadopago.configure({
+  access_token: process.env.ACCESS_TOKE || "",
+});
+
+//ruta mercado pago
 Mercado_Pago.post("/", async (req, res) => {
-    try{
+  try {
+    const proference = {
+    //cada objeto representa un producto
+      items: [
+        {
+          title: "Computadora",
+          picture_url: "https://qseyo",
+          unit_price: 200,
+          currrency_id: "USD",
+          description: "descripcions",
+          quantity: 1,
+        },
+      ],
+      //ruta por si falla el proceso
+      back_urls: {
+        sussecs: "",
+        fiilure: "",
 
-    }catch (error) {
-        console.error(error.message);
-        res.status(500).json.(error.message);
-    }
-})
+      },
+      //ruta por si sale bien
+      auto_return: "aproved",
+    };
+//promesa para esperas las respuestas
+
+const respuesta = await mercadopago.preference.create(preference);
+console.log(respuesta);
+res.status(200).json(respuesta);
 
 
+
+
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json(error.message);
+  }
+});
 
 Module.export = Mercado_Pago;
